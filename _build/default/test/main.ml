768b2e584af4da5fc7f13a0d228c942cf11bf743
Nicholas Varela <nicholasjv@icloud.com>
2022-10-21 15:23:42 -0400

    Created tests for dirs and exapnded test world map json
    - dirs test is currently called exits_test
    - map is in data/demo_game.json

--- _build/default/test/main.ml
+++ _build/default/test/main.ml
@@ -1,10 +1,44 @@
 open OUnit2
 open Game.Adventure
 
+(** [cmp_set_like_lists lst1 lst2] compares two lists to see whether they are
+    equivalent set-like lists. That means checking two things. First, they must
+    both be "set-like", meaning that they do not contain any duplicates. Second,
+    they must contain the same elements, though not necessarily in the same
+    order. *)
+(* let cmp_set_like_lists lst1 lst2 = let uniq1 = List.sort_uniq compare lst1 in
+   let uniq2 = List.sort_uniq compare lst2 in List.length lst1 = List.length
+   uniq1 && List.length lst2 = List.length uniq2 && uniq1 = uniq2 *)
+
+(** [pp_string s] pretty-prints string [s]. *)
+let pp_string s = "\"" ^ s ^ "\""
+
+(** [pp_list pp_elt lst] pretty-prints list [lst], using [pp_elt] to
+    pretty-print each element of [lst]. *)
+let pp_list pp_elt lst =
+  let pp_elts lst =
+    let rec loop n acc = function
+      | [] -> acc
+      | [ h ] -> acc ^ pp_elt h
+      | h1 :: (_ :: _ as t') ->
+          if n = 100 then acc ^ "..." (* stop printing long list *)
+          else loop (n + 1) (acc ^ pp_elt h1 ^ "; ") t'
+    in
+    loop 0 "" lst
+  in
+  "[" ^ pp_elts lst ^ "]"
+
+(***asdfasdfs*)
+
 let prefix = "data" ^ Filename.dir_sep
 let rhodes = Yojson.Basic.from_file (prefix ^ "demo_game.json")
 let rhodes = from_json rhodes
 let start_test (name : string) : test = name >:: fun _ -> assert_equal true true
+
+let exits_test (name : string) (a : t) (s : string)
+    (expected_output : string list) : test =
+  name >:: fun _ ->
+  assert_equal expected_output (dirs a s) ~printer:(pp_list pp_string)
 
 let initial_room_test (name : string) (a : t) (expected_output : string) : test
     =
@@ -13,7 +47,12 @@ let initial_room_test (name : string) (a : t) (expected_output : string) : test
 let basic_tests = [ start_test "n" ]
 
 let adventure_tests =
-  [ initial_room_test "Inital Room of Rhodes is [lobby]" rhodes "lobby" ]
+  [
+    initial_room_test "Inital Room of Rhodes is [lobby]" rhodes "lobby";
+    exits_test "Exits of Lobby" rhodes "lobby" [ "cs lounge"; "upson" ];
+    exits_test "Exits of Rhodes CS Lounge" rhodes "cs lounge" [ "lobby" ];
+    exits_test "Exits of upson" rhodes "upson" [ "rhodes" ];
+  ]
 
 let suite =
   "test suite for Final Project"
